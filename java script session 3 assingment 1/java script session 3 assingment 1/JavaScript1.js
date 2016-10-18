a0aefcbc92c06e5cb7b18037685e317e77c403d7
praveenearle <praveen.earle@gmail.com>
2016-10-18 09:58:09 +0530

    java script s3a1
    java script s3a1

---
+++ java script session 3 assingment 1/java script session 3 assingment 1/JavaScript1.js
@@ -0,0 +1,9 @@
+﻿function like() {
+    if (document.getElementById("myButton1").value === 'Like') {
+        document.getElementById("myButton1").value = "UnLike";
+    } else {
+        document.getElementById("myButton1").value = "Like";
+    }
+    
+
+}
